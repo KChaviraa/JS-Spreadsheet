ac9a96db9eff25d3d621a0220562ce145196e47f
Kate Chavira <143099677+KChaviraa@users.noreply.github.com>
2024-05-02 15:05:18 -0500

    Updated code with the Core function that evaluates input in a spreadsheet-like manner, included key functions such as infixToFunction which includes the arthimetic operators and the spreadsheetFunctions which are essential to this project

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+
+//Object that maps arithmetic operators
+const infixToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y,
+}
+
+//Function that uses regex to find matches in the string(str) representing infix notation expression
+//ex:(3+5), then it replaces each match with the result of evaluating the corresponding arithmetic eperation 
+//using the function 'infixToFunction'
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+
+//Responsible for evaluating expressions with high precedence operators
+const highPrecedence = str => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+}
+
+//function that checks if a number is even
 const isEven = num => num % 2 === 0;
+
+//function that calculates the sum of an array of numbers
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+
+// calculates the average of an array of numbers
 const average = nums => sum(nums) / nums.length;
 
+// calculates the median of an array of numbers
 const median = nums => {
   const sorted = nums.slice().sort((a, b) => a - b);
   const length = sorted.length;
@@ -11,21 +39,60 @@ const median = nums => {
     : sorted[Math.ceil(middle)];
 }
 
+// object containing various spreadsheet-like functions. These functions operate on arrays of numbers
 const spreadsheetFunctions = {
+  ' ': args => args,
   sum,
   average,
-  median
+  median,
+  even: nums => nums.filter(isEven),
+  someeven: nums => nums.some(isEven),
+  everyeven: nums => nums.every(isEven),
+  firsttwo: nums => nums.slice(0, 2),
+  lasttwo: nums => nums.slice(-2),
+  has2: nums => nums.includes(2),
+  increment: nums => nums.map(num => num + 1),
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  range: nums => range(...nums),
+  nodupes: nums => [...new Set(nums).values()]
 }
 
+// This function takes a string representing a mathematical expression or functional call. It evaluates any mathematical
+//expressions in the string and then applies any spreadsheet functions to the resulting array of numbers. 
+const applyFunction = str => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = args => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
+}
+
+//Generates an array of numbers withing a specified range
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+// generates an array of characters within a specified range
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
+//This is the core function for evaluating formulas in a spreadsheet-like manner.
+// - takes a formula string and replaces cell references with their corresponding values
+// - expands ranges into individual call references
+// - applies any functions or operations present in the formula 
 const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
-
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
+  const cellRegex = /[A-J][1-9][0-9]?/gi;
+  const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+  const functionExpanded = applyFunction(cellExpanded);
+  return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
+// Event handler that sets up the UI for the spreadsheet when the window loads
+// It creates input fields for each cell in a grid-like fashion
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
@@ -49,10 +116,12 @@ window.onload = () => {
   })
 }
 
+//Function that is called whenever the value of an input field changes. 
+// If the value starts with an '=', indicating a formula, it evaluates the formula and updates the input field with the result.
 const update = event => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
   if (!value.includes(element.id) && value.startsWith('=')) {
-
+    element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
   }
 }
